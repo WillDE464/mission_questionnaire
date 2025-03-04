cfd45e0d2c371fb8d8570654adf8a2890acbe672
William <dwilli347@gmail.com>
2025-03-04 12:01:20 +0100

    Chargement du fichier json en ligne de commande

--- questionnaire.py
+++ questionnaire.py
@@ -19,6 +19,9 @@
 #    - lancer()
 #
 import json
+import sys
+
+
 
 class Question:
     def __init__(self, titre, choix, bonne_reponse):
@@ -77,6 +80,20 @@ class Questionnaire:
         questions= [Question.FromJsonData(i) for i in questionnaire_data_questions]
         return Questionnaire(questions, data["categorie"], data["titre"], data["difficulte"])
 
+    def from_json_file(filename): 
+        try:
+            file= open(filename, "r")
+            json_data=file.read()
+            file.close()
+            questionnaire_data= json.loads(json_data)
+            
+        except:
+            print("Erreur lors de l'ouverture ou de la lecture du fichier")
+            return None
+        return Questionnaire.from_json_data(questionnaire_data) 
+        
+
+
     def lancer(self):
         score = 0
         nb_questions= len(self.questions)
@@ -120,11 +137,16 @@ Questionnaire(
 ).lancer()
 """
 
-filename= "cinema_starwars_debutant.json"
-file= open(filename, "r")
-json_data=file.read()
-file.close()
-questionnaire_data= json.loads(json_data)
 
+#Questionnaire.from_json_file("animaux_leschats_confirme.json").lancer()
 
-Questionnaire.from_json_data(questionnaire_data).lancer()
+print(sys.argv)
+
+if len(sys.argv)<2:
+    print("ERREUR : vous devez spécifier le nom du fichier json à charger")
+    exit(0)
+
+json_filename= sys.argv[1]
+questionnaire=Questionnaire.from_json_file(json_filename)
+if questionnaire:
+    questionnaire.lancer()
